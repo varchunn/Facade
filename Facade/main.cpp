2dcd4647d7a16194cf90fe400e5d563c3c3178e4
varchunn <varvaram186@gmail.com>
2025-04-11 20:15:57 +0300

    Initial Commit

---
+++ Facade/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Facade
+//
+//  Created by Варя Мінєнкова on 11.04.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
